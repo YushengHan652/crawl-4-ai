6ed192dca82125f646d8f46ded127bf1110b3377
YushengHan652 <han.yus@northeastern.edu>
2025-03-28 12:50:11 -0400

    1

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# config.py
+
+BASE_URL = "https://play.google.com/store/search?q=digital%20health&c=apps"
+CSS_SELECTOR = "[class^='info-container']"
+REQUIRED_KEYS = [
+    "product_name",
+    "company_name",
+    "average_scores",
+    "total_reviews",
+    "customer_name",
+    "review_score",
+    "review_date",
+    "review_text",
+]
---
+++ src/main.py
@@ -0,0 +1,83 @@
+import asyncio
+
+from crawl4ai import AsyncWebCrawler
+from dotenv import load_dotenv
+
+from config import BASE_URL, CSS_SELECTOR, REQUIRED_KEYS
+from utils.data_utils import (
+    save_products_to_csv,
+)
+from utils.scraper_utils import (
+    fetch_and_process_page,
+    get_browser_config,
+    get_llm_strategy,
+)
+
+load_dotenv()
+
+
+async def crawl_products():
+    """
+    Main function to crawl venue data from the website.
+    """
+    # Initialize configurations
+    browser_config = get_browser_config()
+    llm_strategy = get_llm_strategy()
+    session_id = "product_crawl_session"
+
+    # Initialize state variables
+    page_number = 1
+    all_products = []
+    seen_names = set()
+
+    # Start the web crawler context
+    # https://docs.crawl4ai.com/api/async-webcrawler/#asyncwebcrawler
+    async with AsyncWebCrawler(config=browser_config) as crawler:
+        while True:
+            # Fetch and process data from the current page
+            products, no_results_found = await fetch_and_process_page(
+                crawler,
+                page_number,
+                BASE_URL,
+                CSS_SELECTOR,
+                llm_strategy,
+                session_id,
+                REQUIRED_KEYS,
+                seen_names,
+            )
+
+            if no_results_found:
+                print("No more product found. Ending crawl.")
+                break  # Stop crawling when "No Results Found" message appears
+
+            if not products:
+                print(f"No products extracted from page {page_number}.")
+                break  # Stop if no products are extracted
+
+            # Add the products from this page to the total list
+            all_products.extend(products)
+            page_number += 1  # Move to the next page
+
+            # Pause between requests to be polite and avoid rate limits
+            await asyncio.sleep(2)  # Adjust sleep time as needed
+
+    # Save the collected venues to a CSV file
+    if all_products:
+        save_products_to_csv(all_products, "complete_venues.csv")
+        print(f"Saved {len(all_products)} venues to 'complete_venues.csv'.")
+    else:
+        print("No venues were found during the crawl.")
+
+    # Display usage statistics for the LLM strategy
+    llm_strategy.show_usage()
+
+
+async def main():
+    """
+    Entry point of the script.
+    """
+    await crawl_products()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
